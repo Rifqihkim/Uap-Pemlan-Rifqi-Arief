f641a389dc136119a2e1e5d23582f7b4943d5558
Rifqihkim <rifqhkm@gmail.com>
2025-12-26 11:24:11 +0700

    Class MoodManager.java

--- src/main/java/org/example/MoodManager.java
+++ src/main/java/org/example/MoodManager.java
@@ -6,43 +6,39 @@ import java.util.ArrayList;
 public class MoodManager {
 
     public static ArrayList<Mood> moods = new ArrayList<>();
-    private static final String DIR_NAME = "data";
-    private static final String FILE_NAME = "data/moods.csv";
+    private static final String DIR = "data";
+    private static final String FILE = "data/moods.csv";
 
     public static void loadData() {
         moods.clear();
         try {
-            File file = new File(FILE_NAME);
+            File file = new File(FILE);
             if (!file.exists()) return;
 
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
-
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(",", 3);
-                moods.add(new Mood(data[0], data[1], data[2]));
+                String[] d = line.split(",", 3);
+                moods.add(new Mood(d[0], d[1], d[2]));
             }
             br.close();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Gagal load data!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Gagal load data");
         }
     }
 
     public static void saveData() {
         try {
-            File dir = new File(DIR_NAME);
+            File dir = new File(DIR);
             if (!dir.exists()) dir.mkdir();
 
-            File file = new File(FILE_NAME);
-            if (!file.exists()) file.createNewFile();
-
-            PrintWriter pw = new PrintWriter(new FileWriter(file));
+            PrintWriter pw = new PrintWriter(new FileWriter(FILE));
             for (Mood m : moods) {
                 pw.println(m.toCSV());
             }
             pw.close();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Gagal menyimpan CSV!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Gagal simpan CSV");
         }
     }
 }
