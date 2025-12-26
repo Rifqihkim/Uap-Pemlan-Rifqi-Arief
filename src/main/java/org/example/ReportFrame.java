988e80bdc5f46e46ffd93d3e995eae6750ce81f4
Rifqihkim <rifqhkm@gmail.com>
2025-12-26 11:27:36 +0700

    Class ReportFrame.javaFrame.java

--- src/main/java/org/example/ReportFrame.java
+++ src/main/java/org/example/ReportFrame.java
@@ -24,28 +24,3 @@ public class ReportFrame extends JFrame {
         setVisible(true);
     }
 }
-import javax.swing.*;
-import java.util.HashMap;
-
-public class ReportFrame extends JFrame {
-
-    public ReportFrame() {
-        setTitle("Laporan Mood");
-        setSize(300, 300);
-        setLocationRelativeTo(null);
-
-        HashMap<String, Integer> report = new HashMap<>();
-
-        for (Mood m : MoodManager.moods) {
-            report.put(m.getMood(),
-                    report.getOrDefault(m.getMood(), 0) + 1);
-        }
-
-        JTextArea area = new JTextArea();
-        report.forEach((mood, total) ->
-                area.append(mood + " : " + total + "\n"));
-
-        add(new JScrollPane(area));
-        setVisible(true);
-    }
-}
