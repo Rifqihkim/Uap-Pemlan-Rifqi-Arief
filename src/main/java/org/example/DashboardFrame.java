7eb39061b2535e2ac672be452281ca8ccd65ee33
Rifqihkim <rifqhkm@gmail.com>
2025-12-26 11:22:16 +0700

    Class DashboardFrame.java

--- src/main/java/org/example/DashboardFrame.java
+++ src/main/java/org/example/DashboardFrame.java
@@ -5,20 +5,20 @@ import java.awt.*;
 public class DashboardFrame extends JFrame {
 
     public DashboardFrame() {
-        setTitle("Mood Tracker");
-        setSize(400, 300);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Mood Tracker - Dashboard");
+        setSize(400,300);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JButton btnInput = new JButton("Input Mood");
         JButton btnList = new JButton("List Mood");
-        JButton btnReport = new JButton("Laporan Mood");
+        JButton btnReport = new JButton("Laporan");
 
-        btnInput.addActionListener(e -> new MoodFormFrame());
+        btnInput.addActionListener(e -> new MoodFormFrame(null, -1));
         btnList.addActionListener(e -> new MoodListFrame());
         btnReport.addActionListener(e -> new ReportFrame());
 
-        setLayout(new GridLayout(3, 1, 10, 10));
+        setLayout(new GridLayout(3,1,10,10));
         add(btnInput);
         add(btnList);
         add(btnReport);
